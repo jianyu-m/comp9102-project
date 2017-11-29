5879f7d0bb52f4a6b733682a00406ac9aa4a1796
jianyu-m <jianyu@hku.hk>
2017-11-29 20:59:45 +0800

    nits

--- figures/scale_dataset.py
+++ figures/scale_dataset.py
@@ -2,13 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # evenly sampled time at 200ms intervals
-partition = np.array([1, 2, 3, 4, 5, 6], dtype=np.int)
-gupt_error = np.array([180, 200, 220, 240, 250, 560], dtype=np.float)
-naive_error = np.array([140, 160, 180, 210, 240, 480], dtype=np.float)
-combine_error = np.array([160, 180, 220, 230, 260, 520], dtype=np.float)
+partition = np.array([0, 1, 2, 3, 4, 5], dtype=np.int)
+gupt_error = np.array([180, 190, 220, 252, 295, 350], dtype=np.float)
+naive_error = np.array([140, 160, 186, 200, 230, 270], dtype=np.float)
+combine_error = np.array([160, 180, 210, 246, 280, 320], dtype=np.float)
 
+# locs, labels = xticks()
 
-# billion / 100 million / 80 million / 60 million / 40 million / 20 mullion
+plt.xticks(range(6), ('20M', '40M', '60M', '80M', '100M', '120M') )
+
+# 130million / 100 million / 80 million / 60 million / 40 million / 20 mullion
 axes = plt.gca()
 # axes.set_ylim([0, 100])
 
@@ -18,7 +21,7 @@ plt.plot(partition, naive_error, 'x-', label="PowerGraph")
 plt.plot(partition, combine_error, 'v-', label="GraphX")
 
 plt.ylabel("Time (s)")
-plt.xlabel("Number of Worker")
+plt.xlabel("Number of Edges")
 legend = axes.legend(loc='upper right', shadow=False)
 
 plt.savefig('scale_dataset.pdf', format='pdf', dpi=500)
